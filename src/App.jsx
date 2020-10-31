796618185c5275b4ea771d8b694fe685ccb37b54
KozaX <55134615+KozaX@users.noreply.github.com>
2020-10-31 09:37:01 +0100

    add div to play around with, add react transitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Transition from 'react-transition-group/Transition';
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -9,7 +10,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      modalIsOpen: false
+      modalIsOpen: false,
+      showBlock: false
     }
   }
 
@@ -23,6 +25,14 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
+        <button onClick={()=>this.setState(prevState=> ({showBlock: !prevState.showBlock}))}>Toggle</button>
+        <br/>
+
+        <Transition in={this.state.showBlock} timeout={300}>
+          {state=> <div style={{backgroundColor: 'red', width: 100, height: 100, margin: 'auto', opacity: state === 'exited' ? 0 : 1}}></div>}
+          
+        </Transition>
+
         <Modal show={this.state.modalIsOpen} closed={this.setModal}/>
         <Backdrop show={this.state.modalIsOpen} />
         <button className="Button" onClick={this.setModal}>Open Modal</button>
